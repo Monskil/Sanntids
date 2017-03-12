c81f52aa07d1aa7d992b143755c24e2c8379d2e7
Student <heyword94@gmail.com>
2017-03-12 10:15:54 +0100

    Serverkvinn + klientmann = sant

--- Monskil/Heis/Network/TCP_client_2.go
+++ Monskil/Heis/Network/TCP_client_2.go
@@ -8,14 +8,14 @@ import (
 	"time"
 )
 
-func Orders_to_string_1() string {
+func Orders_to_string_2() string {
 
 	test_inner := [4]int{0, 0, 0, 0}
 	test_outer := [4][2]int{
 		{0, 0},
-		{1, 0},
 		{0, 0},
 		{0, 0},
+		{0, 1},
 	}
 
 	var Orders string = "" //UUUUDDDDCCCC (U = orders button_up | D = orders button_down | C = orders button_command)
@@ -51,9 +51,9 @@ func Orders_to_string_1() string {
 //LABPLASS 06 = 146
 //LABPLASS 14 = 142
 //LABPLASS 16 = 147
-func Network_client_main( /*New_order bool*/ ) {
+func Network_client_main_2( /*New_order bool*/ ) {
 	// connect to this socket
-	conn, err := net.Dial("tcp" /*,"129.241.187.142:1201" */, "localhost:1201")
+	conn, err := net.Dial("tcp" /*,"129.241.187.142:1201" */, "localhost:8081")
 	//fmt.Println(conn)
 
 	/*var monvar net.Conn = (&{{0xc82005a150}})
@@ -72,7 +72,7 @@ func Network_client_main( /*New_order bool*/ ) {
 			//fmt.Println(Orders_to_string_1())
 			//fmt.Println("Client")
 			time.Sleep(300 * time.Millisecond)
-			fmt.Fprintf(conn, Orders_to_string_1())
+			fmt.Fprintf(conn, Orders_to_string_2())
 		}
 	}
 }
--- Monskil/Heis/Network/TCP_server_2.go
+++ Monskil/Heis/Network/TCP_server_2.go
@@ -10,7 +10,7 @@ import (
 
 ///IP_ADRESSE 129.241.187.tall
 
-func String_to_orders(Orders1 string) [4][3]int {
+func String_to_orders_2(Orders1 string) [4][3]int {
 	//fmt.Println(Orders1)
 	//var Orders int = [12] "000000000000" //UUUUDDDDCCCC (U = orders button_up | D = orders button_down | C = orders button_command)
 	var Orders_list = [4] /*N_FLOORS*/ [3] /*N_BUTTONS*/ int{
@@ -51,18 +51,18 @@ func String_to_orders(Orders1 string) [4][3]int {
 	return Orders_list
 }
 
-var allClients map[*Client]int
+/*var allClients map[*Client]int*/
 
-type Client struct {
+/*type Client struct {
 	// incoming chan string
 	outgoing   chan string
 	reader     *bufio.Reader
 	writer     *bufio.Writer
 	conn       net.Conn
 	connection *Client
-}
+}*/
 
-func (client *Client) Read() {
+func (client *Client) Read_2() {
 	for {
 		line, err := client.reader.ReadString('\n')
 		if err == nil {
@@ -82,19 +82,19 @@ func (client *Client) Read() {
 	client = nil
 }
 
-func (client *Client) Write() {
+func (client *Client) Write_2() {
 	for data := range client.outgoing {
 		client.writer.WriteString(data)
 		client.writer.Flush()
 	}
 }
 
-func (client *Client) Listen() {
-	go client.Read()
-	go client.Write()
+func (client *Client) Listen_2() {
+	go client.Read_2()
+	go client.Write_2()
 }
 
-func NewClient(connection net.Conn) *Client {
+func NewClient_2(connection net.Conn) *Client {
 	writer := bufio.NewWriter(connection)
 	reader := bufio.NewReader(connection)
 
@@ -105,20 +105,20 @@ func NewClient(connection net.Conn) *Client {
 		reader:   reader,
 		writer:   writer,
 	}
-	client.Listen()
+	client.Listen_2()
 	return client
 }
 
-func Network_server_main( /*New_order bool*/ ) {
+func Network_server_main_2( /*New_order bool*/ ) {
 	allClients = make(map[*Client]int)
-	listener, _ := net.Listen("tcp", ":1201")
+	listener, _ := net.Listen("tcp", ":8081")
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println(err.Error())
 		}
 
-		client := NewClient(conn)
+		client := NewClient_2(conn)
 		for clientList := range allClients {
 			if clientList.connection == nil {
 				client.connection = clientList
@@ -128,19 +128,19 @@ func Network_server_main( /*New_order bool*/ ) {
 			allClients[client] = 1
 			fmt.Println(len(allClients))
 		}
-		go handleClient(conn /*, New_order*/)
+		go handleClient_2(conn /*, New_order*/)
 	}
 }
 
 //var lol2 bool = Driver.Bursdagskvinn()
-var Server_list = [4] /*N_FLOORS*/ [3] /*N_BUTTONS*/ int{
+var Server_list_2 = [4] /*N_FLOORS*/ [3] /*N_BUTTONS*/ int{
 	{0, 0, 0},
 	{0, 0, 0},
 	{0, 0, 0},
 	{0, 0, 0},
 }
 
-func handleClient(conn net.Conn /*, New_order bool*/) {
+func handleClient_2(conn net.Conn /*, New_order bool*/) {
 	for {
 		defer conn.Close()
 		var buf [12] /*512*/ byte
@@ -155,14 +155,14 @@ func handleClient(conn net.Conn /*, New_order bool*/) {
 			}
 			var x string = string(buf[0:]) // + string('\n')
 			fmt.Println(x)
-			Server_list = /*fmt.Println(*/ String_to_orders(x) //)
+			Server_list_2 = /*fmt.Println(*/ String_to_orders_2(x) //)
 			//fmt.Println(Server_list)
 			//fmt.Println(String_to_orders(x))
 		}
 	}
 }
 
-func checkError(err error) {
+func checkError_2(err error) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
 		os.Exit(1)
